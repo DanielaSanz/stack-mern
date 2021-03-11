e7c73a544c1c087ac3a0302db61209767be4e4bb
Daniela Sanchez <47358877+reinamarla@users.noreply.github.com>
2021-03-11 15:59:08 -0300

    se conecta a la ddbb mongo, se configura el entrono de desarrollo

---
+++ Stack Mern/backend/src/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+//Permite conectarse a una instancia/un servidor de mongodb
+//Protocolo mongodb
+//Se saca la url y se llama a process que permite acceder a todo el sistema de tu pc.
+//Ternario para evitar error en conexion
+console.log(process.env.MONGODB_URI)
+const URI = process.env.MONGODB_URI 
+    ? process.env.MONGODB_URI 
+    : 'mongodb://localhost/databasetest';
+
+mongoose.connect(URI, {
+    //Para conectar
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+//Cuando se conecte para escuchar 
+const conection = mongoose.connection;
+
+//Una vez abierta esta coneccion quiero ver el console.log
+conection.once('open', () => {
+    console.log('DDBB is connected');
+})
--- Stack Mern/backend/src/index.js
+++ Stack Mern/backend/src/index.js
@@ -1,7 +1,12 @@
-const app = require('./app');
+//Para llamar a .env y no mostrar la url. Antes de iniciar ya estan disponibles
+require('dotenv').config();
 
-async function main () {
-    await app.listen(4000);
+const app = require('./app');
+//Llama la conección a la DB
+require('./database');
+
+async function main() {
+    const result = await app.listen(4000);
     console.log('Server on port 4000');
 }
 
