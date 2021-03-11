aaa6e2e294ee55e6b0ca1e3f1156be3c6e157958
Daniela Sanchez <47358877+reinamarla@users.noreply.github.com>
2021-03-11 16:28:11 -0300

    se configura ap.js. Setting, middleware y routes

--- Stack Mern/backend/src/app.js
+++ Stack Mern/backend/src/app.js
@@ -1,5 +1,20 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+//Settings service 
+//Configuracion del puerto, despues lo llamo en index.js
+app.set('port', process.env.PORT || 4000);
+
+
+//Middleware
+app.use(cors());
+//Para que entienda formato json
+app.use(express.json());
+
+
+//Routes
+app.get('/api/users', (req, res) => res.send("Users Routes"));
+app.get('/api/notes', (req, res) => res.send("Notes Routes"));
 
 module.exports = app;
--- Stack Mern/backend/src/index.js
+++ Stack Mern/backend/src/index.js
@@ -6,8 +6,8 @@ const app = require('./app');
 require('./database');
 
 async function main() {
-    const result = await app.listen(4000);
-    console.log('Server on port 4000');
+    await app.listen(app.get('port'));
+    console.log('Server on port', app.get('port'));
 }
 
 main();
